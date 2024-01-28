068ad4e480435ce8e3fd394f10791f281ae0aa78
KoderKareem <abdulkarimsunusi2001@gmail.com>
2024-01-28 15:10:54 +0000

    added keyboard envent on the numbers only

--- script.js
+++ script.js
@@ -13,10 +13,149 @@ function divide(firstNum, secondNum) {
   return firstNum / secondNum;
 }
 
-let firstNum;
-let operators;
-let secondNum;
-let nextValue
+let firstNum = "";
+let operator = "";
+let secondNum = "";
+let currentDiv = document.createElement("div");
+let displayContent = document.querySelector("#displayContent");
+
+// Number buttons
+let numbers = document.querySelectorAll("#number");
+numbers.forEach((number) => {
+  number.addEventListener("click", updateNumber);
+});
+
+function updateNumber(e) {
+  currentNum = e.target.textContent;
+  currentDiv.textContent += currentNum;
+  displayContent.appendChild(currentDiv);
+}
+let body = document.querySelector("body");
+
+// body.forEach((number) => {
+body.addEventListener("keydown", (ev) => {
+  switch (ev.key) {
+    case "1":
+      currentNum = ev.key;
+      currentDiv.textContent += currentNum;
+      displayContent.appendChild(currentDiv);
+      break;
+    case "2":
+      currentNum = ev.key;
+      currentDiv.textContent += currentNum;
+      displayContent.appendChild(currentDiv);
+      break;
+    case "3":
+      currentNum = ev.key;
+      currentDiv.textContent += currentNum;
+      displayContent.appendChild(currentDiv);
+      break;
+    case "4":
+      currentNum = ev.key;
+      currentDiv.textContent += currentNum;
+      displayContent.appendChild(currentDiv);
+      break;
+    case "5":
+      currentNum = ev.key;
+      currentDiv.textContent += currentNum;
+      displayContent.appendChild(currentDiv);
+      break;
+    case "6":
+      currentNum = ev.key;
+      currentDiv.textContent += currentNum;
+      displayContent.appendChild(currentDiv);
+      break;
+    case "7":
+      currentNum = ev.key;
+      currentDiv.textContent += currentNum;
+      displayContent.appendChild(currentDiv);
+      break;
+    case "8":
+      currentNum = ev.key;
+      currentDiv.textContent += currentNum;
+      displayContent.appendChild(currentDiv);
+      break;
+    case "9":
+      currentNum = ev.key;
+      currentDiv.textContent += currentNum;
+      displayContent.appendChild(currentDiv);
+      break;
+    case "0":
+      currentNum = ev.key;
+      currentDiv.textContent += currentNum;
+      displayContent.appendChild(currentDiv);
+      break;
+    default:
+      break;
+  }
+});
+// });111
+// function updateNumberKeyBoard(ev){
+
+// }
+// Operator buttons
+const operators = document.querySelectorAll("#operator");
+operators.forEach((op) => {
+  op.addEventListener("click", handleOperator);
+});
+
+function handleOperator(e) {
+  if (firstNum === "") {
+    // If no firstNum, set it to the current display content
+    firstNum = currentDiv.textContent;
+    operator = e.target.textContent;
+    // currentDiv.textContent = operator;
+
+    // Reset the currentDiv for the second number
+    currentDiv.textContent = "";
+  } else {
+    // If firstNum is already set, perform the previous operation and update operator
+    secondNum = currentDiv.textContent;
+    performOperation();
+    operator = e.target.textContent;
+    currentDiv.textContent = operator;
+    displayContent.appendChild(currentDiv);
+
+    // Reset the currentDiv for the new second number
+    currentDiv.textContent = "";
+  }
+}
+
+// Equal button
+const equalBtn = document.querySelector("#equal");
+equalBtn.addEventListener("click", performOperation);
+
+function performOperation() {
+  if (firstNum !== "" && operator !== "" && currentDiv.textContent !== "") {
+    secondNum = currentDiv.textContent;
+    let result = operate(parseFloat(firstNum), operator, parseFloat(secondNum));
+    currentDiv.textContent = `= ${result}`;
+    displayContent.appendChild(currentDiv);
+    // Reset variables for the next operation
+    firstNum = result.toString();
+    operator = "";
+    secondNum = "";
+  }
+}
+
+// BackSpace button
+let backSpace = document.querySelector("#backSpace");
+backSpace.addEventListener("click", () => {
+  currentDiv.textContent = currentDiv.textContent.slice(0, -1);
+});
+// Clear button
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", clearDisplay);
+
+function clearDisplay() {
+  currentDiv.textContent = "";
+  displayContent.innerHTML = "";
+  firstNum = "";
+  operator = "";
+  secondNum = "";
+}
+
+// operate function
 function operate(firstNum, operator, secondNum) {
   switch (operator) {
     case "+":
@@ -27,284 +166,7 @@ function operate(firstNum, operator, secondNum) {
       return firstNum * secondNum;
     case "/":
       return firstNum / secondNum;
-
     default:
       return "Invalid Operator";
   }
-
-  // return divide(firstNum, secondNum);
 }
-// console.log(operate(2, "-", 5));
-// console.log(operate(2, "*", 5));
-// console.log(operate(2, "/", 5));
-// console.log(operate(2, "+", 5));
-let displayContent = document.querySelector("#displayContent");
-let currentNumber = "";
-// let currentNumber2 = "";
-let currentDiv = document.createElement("div");
-// let currentDiv2 = document.createElement("div");
-// update the content of zero
-let zero = document.querySelector("#zero");
-zero.addEventListener("click", updateZero);
-
-function updateZero(e) {
-  currentNumber += zero.textContent;
-  currentDiv.textContent = currentNumber;
-  displayContent.appendChild(currentDiv);
-}
-
-// update the content of one
-let one = document.querySelector("#one");
-one.addEventListener("click", updateOne);
-
-function updateOne(e) {
-  currentNumber += one.textContent;
-  currentDiv.textContent = currentNumber;
-  displayContent.appendChild(currentDiv);
-}
-
-// update the content of two
-let two = document.querySelector("#two");
-two.addEventListener("click", updateTwo);
-
-function updateTwo(e) {
-  currentNumber += two.textContent;
-  currentDiv.textContent = currentNumber;
-  displayContent.appendChild(currentDiv);
-}
-
-// update the content of three
-let three = document.querySelector("#three");
-three.addEventListener("click", updateThree);
-
-function updateThree(e) {
-  currentNumber += three.textContent;
-  currentDiv.textContent = currentNumber;
-  displayContent.appendChild(currentDiv);
-}
-
-// update the content of three
-let four = document.querySelector("#four");
-four.addEventListener("click", updateFour);
-
-function updateFour(e) {
-  currentNumber += four.textContent;
-  currentDiv.textContent = currentNumber;
-  displayContent.appendChild(currentDiv);
-}
-
-// update the content of five
-let five = document.querySelector("#five");
-five.addEventListener("click", updateFive);
-
-function updateFive(e) {
-  currentNumber += five.textContent;
-  currentDiv.textContent = currentNumber;
-  displayContent.appendChild(currentDiv);
-}
-
-// update the content of six
-let six = document.querySelector("#six");
-six.addEventListener("click", updateSix);
-
-function updateSix(e) {
-  currentNumber += six.textContent;
-  currentDiv.textContent = currentNumber;
-  displayContent.appendChild(currentDiv);
-}
-
-// update the content of seven
-let seven = document.querySelector("#seven");
-seven.addEventListener("click", updateSeven);
-
-function updateSeven(e) {
-  currentNumber += seven.textContent;
-  currentDiv.textContent = currentNumber;
-  displayContent.appendChild(currentDiv);
-}
-
-// update the content of eight
-let eight = document.querySelector("#eight");
-eight.addEventListener("click", updateEight);
-
-function updateEight(e) {
-  currentNumber += eight.textContent;
-  currentDiv.textContent = currentNumber;
-  displayContent.appendChild(currentDiv);
-}
-
-// update the content of nine
-let nine = document.querySelector("#nine");
-nine.addEventListener("click", updateNine);
-
-function updateNine(e) {
-  currentNumber += nine.textContent;
-  currentDiv.textContent = currentNumber;
-  displayContent.appendChild(currentDiv);
-}
-// Store to first Number when an operator button is clicked
-let addBtn = document
-  .querySelector("#addition")
-  addBtn.addEventListener("click", storeToFirstNum);
-
-let subBtn = document
-  .querySelector("#subtraction")
-  .addEventListener("click", storeToFirstNum);
-
-let divideBtn = document
-  .querySelector("#division")
-  .addEventListener("click", storeToFirstNum);
-
-let multiplyBtn = document
-  .querySelector("#multiplication")
-  .addEventListener("click", storeToFirstNum);
-
-function storeToFirstNum() {
-  firstNum = currentDiv.textContent;
-  currentDiv.textContent = "";
-  currentNumber = "";
-}
-
-
-// addBtn.querySelector('click', addTofirst)
-
-function addTofirst(e) {
-  
-  firstNum = currentDiv.textContent;
-  currentDiv.textContent = "";
-  currentNumber = "";
-  let convertFirstNum = parseInt(firstNum);
-  let convertSecondNum = parseInt(secondNum);
-  let result = operate(convertFirstNum, '+', convertSecondNum)
-  currentDiv.textContent = `= ${result}`
-  displayContent.appendChild(currentDiv);
-    
-  // Update firstNum for subsequent operations
-  // firstNum = result;
-
- 
-  console.log(result);
-  
-}
-
-//store number to second number
-let equal = document.querySelector("#equal");
-
-equal.addEventListener("click", storeToSecondNum);
-
-function storeToSecondNum(e) {
-  secondNum = currentDiv.textContent;
-  currentDiv.textContent = "";
-  currentNumber = "";
-}
-
-// clear button
-const clear = document.querySelector("#clear");
-clear.addEventListener("click", clearNumber);
-
-function clearNumber() {
-  currentDiv.remove(currentDiv);
-  currentDiv.textContent = "";
-  currentNumber = "";
-}
-
-// update the content of addition operator
-let addition = document.querySelector("#addition");
-addition.addEventListener("click", updateAddOperator);
-function updateAddOperator(e) {
-  operators = addition.textContent;
-
-  console.log(operators);
-}
-// addition.addEventListener("click", AddOperator);
-// function AddOperator(e) {
-//   if (secondNum != null) {
-//     let convertFirstNum = parseInt(firstNum);
-//     let convertSecondNum = parseInt(secondNum);
-//     newEqual.textContent = `${
-//       "= " + operate(convertFirstNum, "+", convertSecondNum)
-//     }`;
-//     displayContent.appendChild(newEqual);
-//     firstNum = newEqual.textContent;
-//     let newAdd = document.createElement("div");
-//     newAdd.textContent = operators;
-
-//     console.log(operate(convertFirstNum, "+", convertSecondNum));
-//   }
-// }
-// update the content of operator
-let subtraction = document.querySelector("#subtraction");
-subtraction.addEventListener("click", updateSubtractOperator);
-function updateSubtractOperator(e) {
-  operators = subtraction.textContent;
-
-currentDiv.textContent = operators;
-  displayContent.appendChild(currentDiv);
-  console.log(operators);
-}
-
-let division = document.querySelector("#division");
-division.addEventListener("click", updateDivisionOperator);
-function updateDivisionOperator(e) {
-  operators = division.textContent;
-  currentDiv.textContent = operators;
-  displayContent.appendChild(currentDiv)
-
-  console.log(operators);
-}
-
-let multiplication = document.querySelector("#multiplication");
-multiplication.addEventListener("click", updateMultiplicationOperator);
-function updateMultiplicationOperator(e) {
-  operators = multiplication.textContent;
-  currentDiv.textContent = operators;
-  displayContent.appendChild(currentDiv)
-}
-
-equal = document.querySelector("#equal");
-equal.addEventListener("click", total);
-let convertFirstNum = parseInt(firstNum);
-let convertSecondNum = parseInt(secondNum);
-
-function total(e) {
-  if (operators === "+") {
-    let convertFirstNum = parseInt(firstNum);
-    let convertSecondNum = parseInt(secondNum);
-    let result = operate(convertFirstNum, '+', convertSecondNum)
-    currentDiv.textContent = `= ${result}`
-    displayContent.appendChild(currentDiv);
-console.log(result)
-    firstNum = result
-    
-  } else if (operators === "-") {
-    let convertFirstNum = parseInt(firstNum);
-    let convertSecondNum = parseInt(secondNum);
-    currentDiv.textContent = `${
-      "= " + operate(convertFirstNum, "-", convertSecondNum)
-    }`;
-    displayContent.appendChild(currentDiv);
-    // firstNum = newEqual.textContent
-    console.log(operate(convertFirstNum, "-", convertSecondNum));
-  } else if (operators === "*") {
-    let convertFirstNum = parseInt(firstNum);
-    let convertSecondNum = parseInt(secondNum);
-    currentDiv.textContent = `${
-      "= " + operate(convertFirstNum, "*", convertSecondNum)
-    }`;
-    displayContent.appendChild(currentDiv);
-    // firstNum = newEqual.textContent
-    console.log(operate(convertFirstNum, "*", convertSecondNum));
-  } else if (operators === "/") {
-    let convertFirstNum = parseInt(firstNum);
-    let convertSecondNum = parseInt(secondNum);
-    currentDiv.textContent = `${
-      "= " + operate(convertFirstNum, "/", convertSecondNum)
-    }`;
-    displayContent.appendChild(currentDiv);
-    // firstNum = newEqual.textContent
-    console.log(operate(convertFirstNum, "/", convertSecondNum));
-  } else {
-    return Error;
-  }
-}
-
--- trash.js
+++ trash.js
@@ -1,12 +1,345 @@
-currentNumber2 += one.textContent;
+function add(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+function subtract(firstNum, secondNum) {
+  return firstNum - secondNum;
+}
 
-currentDiv2.append(currentNumber2)
-displayContent.appendChild(currentDiv2)
+function multiply(firstNum, secondNum) {
+  return firstNum * secondNum;
+}
 
-let se = document.querySelector("#equal");
-se.addEventListener("click", function () {
+function divide(firstNum, secondNum) {
+  return firstNum / secondNum;
+}
 
-    secondNum = currentDiv2.
-    currentNumber = "";
+let firstNum;
+let operators;
+let secondNum;
+let nextValue
+function operate(firstNum, operator, secondNum) {
+  switch (operator) {
+    case "+":
+      return firstNum + secondNum;
+    case "-":
+      return firstNum - secondNum;
+    case "*":
+      return firstNum * secondNum;
+    case "/":
+      return firstNum / secondNum;
 
-});
+    default:
+      return "Invalid Operator";
+  }
+
+  // return divide(firstNum, secondNum);
+}
+// console.log(operate(2, "-", 5));
+// console.log(operate(2, "*", 5));
+// console.log(operate(2, "/", 5));
+// console.log(operate(2, "+", 5));
+let displayContent = document.querySelector("#displayContent");
+let currentNumber = "";
+// let currentNumber2 = "";
+let currentDiv = document.createElement("div");
+// let currentDiv2 = document.createElement("div");
+// update the content of zero
+let zero = document.querySelector("#zero");
+zero.addEventListener("click", updateZero);
+
+function updateZero(e) {
+  currentNumber += zero.textContent;
+  currentDiv.textContent = currentNumber;
+  displayContent.appendChild(currentDiv);
+}
+
+// update the content of one
+let one = document.querySelector("#one");
+one.addEventListener("click", updateOne);
+
+function updateOne(e) {
+  currentNumber += one.textContent;
+  currentDiv.textContent = currentNumber;
+  displayContent.appendChild(currentDiv);
+}
+
+// update the content of two
+let two = document.querySelector("#two");
+two.addEventListener("click", updateTwo);
+
+function updateTwo(e) {
+  currentNumber += two.textContent;
+  currentDiv.textContent = currentNumber;
+  displayContent.appendChild(currentDiv);
+}
+
+// update the content of three
+let three = document.querySelector("#three");
+three.addEventListener("click", updateThree);
+
+function updateThree(e) {
+  currentNumber += three.textContent;
+  currentDiv.textContent = currentNumber;
+  displayContent.appendChild(currentDiv);
+}
+
+// update the content of three
+let four = document.querySelector("#four");
+four.addEventListener("click", updateFour);
+
+function updateFour(e) {
+  currentNumber += four.textContent;
+  currentDiv.textContent = currentNumber;
+  displayContent.appendChild(currentDiv);
+}
+
+// update the content of five
+let five = document.querySelector("#five");
+five.addEventListener("click", updateFive);
+
+function updateFive(e) {
+  currentNumber += five.textContent;
+  currentDiv.textContent = currentNumber;
+  displayContent.appendChild(currentDiv);
+}
+
+// update the content of six
+let six = document.querySelector("#six");
+six.addEventListener("click", updateSix);
+
+function updateSix(e) {
+  currentNumber += six.textContent;
+  currentDiv.textContent = currentNumber;
+  displayContent.appendChild(currentDiv);
+}
+
+// update the content of seven
+let seven = document.querySelector("#seven");
+seven.addEventListener("click", updateSeven);
+
+function updateSeven(e) {
+  currentNumber += seven.textContent;
+  currentDiv.textContent = currentNumber;
+  displayContent.appendChild(currentDiv);
+}
+
+// update the content of eight
+let eight = document.querySelector("#eight");
+eight.addEventListener("click", updateEight);
+
+function updateEight(e) {
+  currentNumber += eight.textContent;
+  currentDiv.textContent = currentNumber;
+  displayContent.appendChild(currentDiv);
+}
+
+// update the content of nine
+let nine = document.querySelector("#nine");
+nine.addEventListener("click", updateNine);
+
+function updateNine(e) {
+  currentNumber += nine.textContent;
+  currentDiv.textContent = currentNumber;
+  displayContent.appendChild(currentDiv);
+}
+// Store to first Number when an operator button is clicked
+let addBtn = document
+  .querySelector("#addition")
+  addBtn.addEventListener("click", storeToFirstNum);
+
+let subBtn = document
+  .querySelector("#subtraction")
+  .addEventListener("click", storeToFirstNum);
+
+let divideBtn = document
+  .querySelector("#division")
+  .addEventListener("click", storeToFirstNum);
+
+let multiplyBtn = document
+  .querySelector("#multiplication")
+  .addEventListener("click", storeToFirstNum);
+
+function storeToFirstNum() {
+  firstNum = currentDiv.textContent;
+
+  currentDiv.textContent = "";
+  currentNumber = "";
+  
+  
+}
+
+const tokens = expression.replace(/\s/g, '').match(/(\d+|\+|\-|\*|\/)/g);
+
+
+addBtn.addEventListener("click", addTofirst);
+let clickCount = 0;
+
+function addTofirst(e) {
+  for(const token of tokens){
+    if(token === '+'){
+      operators = token
+    } else{
+      // Parse operand
+      const operand = parseInt(token);
+      switch (operators) {
+        case '+':
+          
+          break;
+      
+        default:
+          break;
+      }
+    }
+  }
+  if (firstNum !=  null && secondNum != null) {
+    let convertFirstNum = parseInt(firstNum);
+    let convertSecondNum = parseInt(secondNum);
+ 
+      let result = operate(convertFirstNum, '+', convertSecondNum)
+      currentDiv.textContent = `= ${result}`
+      displayContent.appendChild(currentDiv);
+  console.log(result)
+      firstNum = result
+    
+   
+    
+  } 
+}
+
+
+
+//store number to second number
+let equal = document.querySelector("#equal");
+
+equal.addEventListener("click", storeToSecondNum);
+
+function storeToSecondNum(e) {
+  secondNum = currentDiv.textContent;
+  currentDiv.textContent = "";
+  currentNumber = "";
+}
+
+// clear button
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", clearNumber);
+
+function clearNumber() {
+  currentDiv.remove(currentDiv);
+  currentDiv.textContent = "";
+  currentNumber = "";
+}
+
+// update the content of addition operator
+let addition = document.querySelector("#addition");
+addition.addEventListener("click", updateAddOperator);
+function updateAddOperator(e) {
+  operators = addition.textContent;
+
+  console.log(operators);
+}
+// addition.addEventListener("click", AddOperator);
+// function AddOperator(e) {
+//   if (secondNum != null) {
+//     let convertFirstNum = parseInt(firstNum);
+//     let convertSecondNum = parseInt(secondNum);
+//     newEqual.textContent = `${
+//       "= " + operate(convertFirstNum, "+", convertSecondNum)
+//     }`;
+//     displayContent.appendChild(newEqual);
+//     firstNum = newEqual.textContent;
+//     let newAdd = document.createElement("div");
+//     newAdd.textContent = operators;
+
+//     console.log(operate(convertFirstNum, "+", convertSecondNum));
+//   }
+// }
+// update the content of operator
+let subtraction = document.querySelector("#subtraction");
+subtraction.addEventListener("click", updateSubtractOperator);
+function updateSubtractOperator(e) {
+  operators = subtraction.textContent;
+
+currentDiv.textContent = operators;
+  displayContent.appendChild(currentDiv);
+  console.log(operators);
+}
+
+let division = document.querySelector("#division");
+division.addEventListener("click", updateDivisionOperator);
+function updateDivisionOperator(e) {
+  operators = division.textContent;
+  currentDiv.textContent = operators;
+  displayContent.appendChild(currentDiv)
+
+  console.log(operators);
+}
+
+let multiplication = document.querySelector("#multiplication");
+multiplication.addEventListener("click", updateMultiplicationOperator);
+function updateMultiplicationOperator(e) {
+  operators = multiplication.textContent;
+  currentDiv.textContent = operators;
+  displayContent.appendChild(currentDiv)
+}
+
+equal = document.querySelector("#equal");
+equal.addEventListener("click", total);
+let convertFirstNum = parseInt(firstNum);
+let convertSecondNum = parseInt(secondNum);
+
+function total(e) {
+  if (operators === "+") {
+    let convertFirstNum = parseInt(firstNum);
+    let convertSecondNum = parseInt(secondNum);
+    let result = operate(convertFirstNum, '+', convertSecondNum)
+    currentDiv.textContent = `= ${result}`
+    displayContent.appendChild(currentDiv);
+console.log(result)
+    // firstNum = result
+    
+  } else if (operators === "-") {
+    let convertFirstNum = parseInt(firstNum);
+    let convertSecondNum = parseInt(secondNum);
+    currentDiv.textContent = `${
+      "= " + operate(convertFirstNum, "-", convertSecondNum)
+    }`;
+    displayContent.appendChild(currentDiv);
+    // firstNum = newEqual.textContent
+    console.log(operate(convertFirstNum, "-", convertSecondNum));
+  } else if (operators === "*") {
+    let convertFirstNum = parseInt(firstNum);
+    let convertSecondNum = parseInt(secondNum);
+    currentDiv.textContent = `${
+      "= " + operate(convertFirstNum, "*", convertSecondNum)
+    }`;
+    displayContent.appendChild(currentDiv);
+    // firstNum = newEqual.textContent
+    console.log(operate(convertFirstNum, "*", convertSecondNum));
+  } else if (operators === "/") {
+    let convertFirstNum = parseInt(firstNum);
+    let convertSecondNum = parseInt(secondNum);
+    currentDiv.textContent = `${
+      "= " + operate(convertFirstNum, "/", convertSecondNum)
+    }`;
+    displayContent.appendChild(currentDiv);
+    // firstNum = newEqual.textContent
+    console.log(operate(convertFirstNum, "/", convertSecondNum));
+  } else {
+    return Error;
+  }
+}
+
+case "+":
+  operator = ev.key;
+
+  break;
+case "-":
+  operator = ev.key;
+  break;
+case "/":
+  operator = ev.key;
+  break;
+case "*":
+  operator = ev.key;
+
+  break;
